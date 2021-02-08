d0cd56f6b97a931a43febc1a5c27e8129b233aef
dennvm09 <valenmosquera1999@gmail.com>
2021-02-08 04:02:27 -0500

    modal works, everything works :)))) ; i need to change the profile pic

--- script-cv.js
+++ script-cv.js
@@ -3,19 +3,19 @@ const closeEls = document.querySelectorAll("[data-close]");
 const isVisible = "is-visible";
 
 for (const el of openEls) {
-  el.addEventListener("mouseover", function() {
+  el.addEventListener("click", function() {
     const modalId = this.dataset.open;
     document.getElementById(modalId).classList.add(isVisible);
   });
 }
 
 for (const el of closeEls) {
-  el.addEventListener("mouseover", function() {
+  el.addEventListener("click", function() {
     this.parentElement.parentElement.parentElement.classList.remove(isVisible);
   });
 }
 
-document.addEventListener("mouseover", e => {
+document.addEventListener("click", e => {
   if (e.target == document.querySelector(".modal.is-visible")) {
     document.querySelector(".modal.is-visible").classList.remove(isVisible);
   }
